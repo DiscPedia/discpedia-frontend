25e55dc5b0bb6ee82ca4e9dbc8c97bb03b5e3f91
KIM-korea <dohyun3885@gmail.com>
2026-05-25 16:51:35 +0900

    환경변수하드코딩

--- src/apis/auth/api-config.ts
+++ src/apis/auth/api-config.ts
@@ -1,5 +1,7 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ??
-  (typeof window !== "undefined" && window.location.hostname === "localhost"
-    ? "http://localhost:8080"
-    : "");
+// export const API_BASE_URL =
+//   import.meta.env.VITE_API_BASE_URL ??
+//   (typeof window !== "undefined" && window.location.hostname === "localhost"
+//     ? "http://localhost:8080"
+//     : "");
+
+export const API_BASE_URL = "https://discpedia.roundtable02.com";
